670c86a30947e621b743cd8800e7e5802bd37ffd
ahmaddamr <ahmed.eslam51a@gmail.com>
2024-09-26 15:52:17 +0300

    finish all

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -17,32 +17,33 @@ class SearchView extends StatelessWidget {
         title: const Text('Search a City'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Center(
-            child: TextField(
-          onSubmitted: (value) async {
-            // weatherModel =
-            // await WeatherService().getweather(cityname: value);
-            // log(weatherModel.city);
-            // بيخليك ترجع للصفحه ال قبلها
-            var getcubit = BlocProvider.of<GetWeatherCubit>(context);
-            getcubit.getWeathercubit(cityName: value);
-            Navigator.of(context).pop();
-          },
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 25),
-            labelText: 'Search',
-            suffixIcon: Icon(Icons.search),
-            hintText: 'Enter City Name',
-            border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.deepOrangeAccent,
-                ),
-                borderRadius: BorderRadius.circular(10)),
+          child: TextField(
+            onSubmitted: (value) async {
+              // weatherModel =
+              // await WeatherService().getweather(cityname: value);
+              // log(weatherModel.city);
+              // بيخليك ترجع للصفحه ال قبلها
+              var getcubit = BlocProvider.of<GetWeatherCubit>(context);
+              getcubit.getWeathercubit(cityName: value);
+              Navigator.of(context).pop();
+            },
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 25),
+              labelText: 'Search',
+              suffixIcon: const Icon(Icons.search),
+              hintText: 'Enter City Name',
+              border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.deepOrangeAccent,
+                  ),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
 }
-
